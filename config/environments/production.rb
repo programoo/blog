81b67d26e62826d48c2725f1df7532bd122f071c
prakarn ubuntu pc <prakarn_pc_ubuntu@gmail.com>
2025-11-13 11:21:31 +0700

    Add public_file_server enabled

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
+  # Enable serving of assets
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || true
+
 end
