75acb368647351e8f3065f4e68982dbe5a4e5b53
prakarn ubuntu pc <prakarn_pc_ubuntu@gmail.com>
2025-11-14 17:04:21 +0700

    Show movie images lot of them

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -1,9 +1,6 @@
 class Admin::MoviesController < Admin::ApplicationController
   def index
-    # @pagy, @movies = pagy(Movie.order(created_at: :desc), items: 5)
     @pagy, @movies = pagy(:offset, Movie.order(created_at: :desc), limit: 10)
-
-
   end
 
   def show
