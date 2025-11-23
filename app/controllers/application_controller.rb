09b7c2b1b34cfe6c98f011c1f7013df6958e2da5
prakarn ubuntu pc <prakarn_pc_ubuntu@gmail.com>
2025-11-23 17:59:37 +0700

    Add sign up with first name andn last name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,13 @@ class ApplicationController < ActionController::Base
   include Pagy::Method
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    # For account update (optional)
+    #devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+  end
 end
