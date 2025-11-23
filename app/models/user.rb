fc70e2d4917e80d25af33cb9beb1b79428959043
prakarn ubuntu pc <prakarn_pc_ubuntu@gmail.com>
2025-11-23 15:57:57 +0700

    User profile looks good

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :user_likes, dependent: :destroy
   has_many :liked_movies, through: :user_likes, source: :movie
+  has_one_attached :avatar
 end
