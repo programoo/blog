912675e212029d144babed670c5b878b89b2d234
prakarn ubuntu pc <prakarn_pc_ubuntu@gmail.com>
2025-11-25 06:28:41 +0700

    Notification counter work in real time

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,5 +2,16 @@ class Notification < ApplicationRecord
   belongs_to :user
   belongs_to :notifiable, polymorphic: true
 
-  after_create_commit -> { broadcast_prepend_later_to "notifications_#{user_id}" }
+  after_create_commit :broadcast_notification
+
+  def broadcast_notification
+    # 1. Prepend new notification into the list
+    broadcast_prepend_later_to "notifications_#{user_id}"
+
+    # 2. Update the counter badge
+    broadcast_replace_later_to "notifications_#{user_id}",
+      target: "notification-count",
+      partial: "notifications/count",
+      locals: { user: user }
+  end
 end
