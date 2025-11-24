427df408f3fb08e2ea0b8ef021db16a3d9c69004
prakarn ubuntu pc <prakarn_pc_ubuntu@gmail.com>
2025-11-24 12:01:07 +0700

    Refactor comment panel by using trix

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -69,6 +69,6 @@ class CommentsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def comment_params
-      params.expect(comment: [ :content ])
+      params.expect(comment: [ :content, :criticism ])
     end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :movie, touch: true
   belongs_to :user, optional: true
   has_many :replies, dependent: :destroy
+  has_rich_text :criticism
 end
