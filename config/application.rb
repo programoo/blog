3080a5b808acb343e2409ae75ba01edbe5f87358
prakarn ubuntu pc <prakarn_pc_ubuntu@gmail.com>
2025-11-27 20:03:48 +0700

    First rspect look good

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,16 @@ module Blog
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
 
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: false,
+        request_specs: true
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+    end
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
