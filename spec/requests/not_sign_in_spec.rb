b31784692e72ad719225b12b7d3e93cfee41ea00
prakarn ubuntu pc <prakarn_pc_ubuntu@gmail.com>
2025-11-28 05:54:20 +0700

    Add spec to not sign in page

---
+++ spec/requests/not_sign_in_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe "Welcome", type: :request do
+
+  it "allows access to home" do
+    get "/"
+    expect(response).to have_http_status(:ok)
+  end
+
+  it "allows access to feeds" do
+    get "/feeds"
+    expect(response).to have_http_status(:ok)
+  end
+
+  it "allows access to user sign in" do
+    get "/users/sign_in"
+    expect(response).to have_http_status(:ok)
+  end
+
+end
