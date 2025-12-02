c122a2234056010535286cfb21148e12750fe156
prakarn ubuntu pc <prakarn_pc_ubuntu@gmail.com>
2025-12-02 18:47:33 +0700

    Change log location

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,8 @@ Rails.application.configure do
 
   # Log to STDOUT with the current request id as a default log tag.
   config.log_tags = [ :request_id ]
-  config.logger   = ActiveSupport::Logger.new("/var/log/production.log") #ActiveSupport::TaggedLogging.logger(STDOUT)
+  config.logger = ActiveSupport::Logger.new("log/production.log", 10, 10.megabytes)
+
 
   # Change to "debug" to log everything (including potentially personally-identifiable information!)
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
